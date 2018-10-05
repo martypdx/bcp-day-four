caf80c62d1091f6ca68143b88c3394c31b45b57b
martypdx <marty@martypdx.com>
2018-10-04 20:28:51 -0700

    animal guessing

--- index.js
+++ index.js
@@ -2,19 +2,38 @@
 function handleSubmit(event) {
     var form = event.target;
     var elements = form.elements;
-
-    var name = elements.name.value;
-    console.log('name', name);
-
-    var comments = elements.comments.value;
-    console.log('comments', comments);
-
-    var toppings = elements.toppings.value;
-    console.log('toppings', toppings);
+    var guess = elements.guess.value;
     
-    var grade = elements.grade.value;
-    console.log('grade', grade);
+    var answer = 7;
 
+    var text = '';
+    var color = '';
+
+    
+    if(guess == answer) {
+        text = 'Correct!';
+        color = 'correct';
+    }
+    else if(guess < answer - 3) {
+        text = 'super too low!';
+        color = 'super-low';
+    }
+    else if(guess < answer) {
+        text = 'too low!';
+        color = 'low';
+    }
+    else if(guess > answer + 3) {
+        text = 'super too high!';
+        color = 'super-high';
+    }
+    else {
+        text = 'too high!';
+        color = 'high';
+    }
+
+    var result = document.getElementById('result');
+    result.textContent = text;
+    result.className = color;
 }
 
 
