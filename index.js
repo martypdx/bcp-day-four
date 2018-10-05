f585039e0055832c10e379d3461dcd758185f42f
martypdx <marty@martypdx.com>
2018-10-04 19:24:14 -0700

    get some form values in js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var count = 1;
-function sayHello() {
-    console.log('hello world!, for the ', count, 'time');
-    count++;
+
+function handleSubmit(event) {
+    var form = event.target;
+    var elements = form.elements;
+
+    var name = elements.name.value;
+    console.log('name', name);
+
+    var comments = elements.comments.value;
+    console.log('comments', comments);
+
+    var toppings = elements.toppings.value;
+    console.log('toppings', toppings);
+    
+    var grade = elements.grade.value;
+    console.log('grade', grade);
+
 }
 
 
